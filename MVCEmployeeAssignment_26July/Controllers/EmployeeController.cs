605640fd20c029e0ba073c1a4b5b28e51dc92162
swatidalal <swati15091999@gmail.com>
2022-07-26 16:29:03 +0530

    Add project files.

---
+++ MVCEmployeeAssignment_26July/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _26July_EmployeeMVCAssignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Employee()
+        {
+            return View();
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCEmployeeAssignment_26July/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCEmployeeAssignment_26July.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
